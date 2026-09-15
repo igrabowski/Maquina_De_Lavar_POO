1b809399bb905a0ec6c2f06a130e09bec328997d
HenriqueSMelo27 <henriquesmelo27@gmail.com>
2026-09-15 20:38:33 -0300

    Update MaquinaDeLavar.java

--- src/MaquinaDeLavar.java
+++ src/MaquinaDeLavar.java
@@ -1,35 +1,52 @@
-public class Maquina {
+ublic class Maquina {
 
     String ligada = "nao";
     String porta = "sim";
+    String lavando = "nao";
+    String centrifugando = "nao";
 
     public void ligar() {
-        if (porta.equals("sim")) {
+
+        if (ligada.equals("sim")) {
+            System.out.println("A máquina já está ligada!");
+        } else {
             ligada = "sim";
             System.out.println("Máquina ligada!");
-        } else {
-            System.out.println("Não é possível ligar. Feche a porta.");
         }
     }
 
     public void desligar() {
-        ligada = "nao";
-        System.out.println("Máquina desligada!");
-    }
 
-    public void fecharPorta() {
-        porta = "sim";
-        System.out.println("Porta fechada!");
-    }
-
-    public void abrirPorta() {
         if (ligada.equals("nao")) {
-            porta = "nao";
-            System.out.println("Porta aberta!");
+            System.out.println("A máquina já está desligada!");
+        } else if (lavando.equals("sim") || lavando.equals("pausada")) {
+            System.out.println("Não é possível desligar enquanto a lavagem estiver em andamento ou pausada.");
+        } else if (centrifugando.equals("sim")) {
+            System.out.println("Não é possível desligar enquanto a máquina estiver centrifugando.");
         } else {
-            System.out.println("Não é possível abrir a porta com a máquina ligada.");
+            ligada = "nao";
+            System.out.println("Máquina desligada!");
         }
     }
 
+    public void fecharPorta() {
+
+        if (porta.equals("sim")) {
+            System.out.println("A porta já está fechada!");
+        } else {
+            porta = "sim";
+            System.out.println("Porta fechada!");
+        }
     }
-}
+
+    public void abrirPorta() {
+
+        if (lavando.equals("sim") || lavando.equals("pausada")) {
+            System.out.println("Não é possível abrir a porta durante a lavagem.");
+        } else if (centrifugando.equals("sim")) {
+            System.out.println("Não é possível abrir a porta durante a centrifugação.");
+        } else {
+            porta = "nao";
+            System.out.println("Porta aberta!");
+        }
+    }
