a137b0e332b23d252da34a565421893badab0cb4
Igor Grabowski <igor.grabowski@univille.br>
2026-09-15 20:40:15 -0300

    Adicionando classes de lavagem

--- src/MaquinaDeLavar.java
+++ src/MaquinaDeLavar.java
@@ -50,3 +50,52 @@ ublic class Maquina {
             System.out.println("Porta aberta!");
         }
     }
+    public void iniciarLavagem() {
+
+        if (ligada.equals("nao")) {
+            System.out.println("Não é possível iniciar a lavagem. A máquina está desligada.");
+        } else if (porta.equals("nao")) {
+            System.out.println("Não é possível iniciar a lavagem. Feche a porta.");
+        } else if (lavando.equals("sim")) {
+            System.out.println("A máquina já está lavando!");
+        } else if (lavando.equals("pausada")) {
+            System.out.println("A lavagem está pausada. Use retomarLavagem().");
+        } else {
+            lavando = "sim";
+            System.out.println("Lavagem iniciada!");
+        }
+    }
+
+    public void pausarLavagem() {
+
+        if (ligada.equals("nao")) {
+            System.out.println("Não é possível pausar. A máquina está desligada.");
+        } else if (!lavando.equals("sim")) {
+            System.out.println("Não é possível pausar. A máquina não está lavando.");
+        } else {
+            lavando = "pausada";
+            System.out.println("Lavagem pausada!");
+        }
+    }
+
+    public void retomarLavagem() {
+
+        if (ligada.equals("nao")) {
+            System.out.println("Não é possível retomar. A máquina está desligada.");
+        } else if (!lavando.equals("pausada")) {
+            System.out.println("Não é possível retomar. A lavagem não está pausada.");
+        } else {
+            lavando = "sim";
+            System.out.println("Lavagem retomada!");
+        }
+    }
+
+    public void concluirLavagem() {
+
+        if (!lavando.equals("sim")) {
+            System.out.println("Não é possível concluir. A máquina não está lavando.");
+        } else {
+            lavando = "nao";
+            System.out.println("Lavagem concluída!");
+        }
+    }
