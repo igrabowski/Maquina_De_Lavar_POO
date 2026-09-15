1b18b354fd58c2797bd28691cf338eb9a9e51f27
Igor Grabowski <igor.grabowski@univille.br>
2026-09-15 20:34:25 -0300

    Atualização da main

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,33 @@ public class Main {
 
         Maquina maquina = new Maquina();
 
-        maquina.fecharPorta();
+        System.out.println("TESTANDO RESTRIÇÕES");
+
+        maquina.iniciarLavagem();
         maquina.ligar();
+        maquina.ligar();
+        maquina.iniciarLavagem();
+        maquina.abrirPorta();
+        maquina.pausarLavagem();
+        maquina.abrirPorta();
+        maquina.desligar();
+        maquina.retomarLavagem();
+        maquina.concluirLavagem();
+        
+        System.out.println();
+        System.out.println("CENTRIFUGAÇÃO");
+
+        maquina.iniciarCentrifugacao();
+        maquina.abrirPorta();
+        maquina.desligar();
+        maquina.concluirCentrifugacao();
+
+        System.out.println();
+        System.out.println("FINALIZANDO");
+
+        maquina.desligar();
         maquina.desligar();
         maquina.abrirPorta();
+        maquina.fecharPorta();
     }
 }
