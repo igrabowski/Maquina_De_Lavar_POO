45b15903e28a7754a64d2502e5e7898e8edbe423
HenriqueSMelo27 <henriquesmelo27@gmail.com>
2026-09-15 20:11:44 -0300

    Refactor MaquinaDeLavar class to Maquina

--- src/MaquinaDeLavar.java
+++ src/MaquinaDeLavar.java
@@ -1,3 +1,35 @@
-public class MaquinaDeLavar {
+public class Maquina {
 
+    String ligada = "nao";
+    String porta = "sim";
+
+    public void ligar() {
+        if (porta.equals("sim")) {
+            ligada = "sim";
+            System.out.println("Máquina ligada!");
+        } else {
+            System.out.println("Não é possível ligar. Feche a porta.");
+        }
+    }
+
+    public void desligar() {
+        ligada = "nao";
+        System.out.println("Máquina desligada!");
+    }
+
+    public void fecharPorta() {
+        porta = "sim";
+        System.out.println("Porta fechada!");
+    }
+
+    public void abrirPorta() {
+        if (ligada.equals("nao")) {
+            porta = "nao";
+            System.out.println("Porta aberta!");
+        } else {
+            System.out.println("Não é possível abrir a porta com a máquina ligada.");
+        }
+    }
+
+    }
 }
