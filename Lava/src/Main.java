b9365aec0b9ca3b880395f2f8df0b8af3e977e43
HenriqueSMelo27 <henriquesmelo27@gmail.com>
2026-09-15 00:37:50 -0300

    Atualiza projeto da maquina de lavar

---
+++ Lava/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Maquina maquina = new Maquina();
+
+        maquina.fecharPorta();
+        maquina.ligar();
+        maquina.desligar();
+        maquina.abrirPorta();
+    }
+}
